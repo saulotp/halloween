29c2a835c0b548c0f408a33ce3e2883f7eff5985
Saulo <saulodetp@gmail.com>
2021-10-26 17:22:09 -0300

    update main js
    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,14 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.getElementById('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 
 /*=============== HOME SWIPER ===============*/
